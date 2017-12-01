8e9ea7dca3ea16bc9640e40d935abe58f4bd1dda
umar1303 <umarchr@mail.ru>
2017-12-01 16:58:17 +0400

    Исправил запрос SELECT

--- u4.php
+++ u4.php
@@ -1,11 +1,11 @@
 <?php
 
-$link = mysqli_connect("localhost","root","root","umar");
-mysqli_set_charset($link,'utf8');
+$link = mysqli_connect( "localhost","root","root","umar" );
+mysqli_set_charset( $link,'utf8' );
 
 function add_group($name,$start_date,$lesson_time,$lesson_days) {
 	global $link;
-	$q = mysqli_query($link, "INSERT INTO groups VALUES($name[name],$start_date[start_date],$lesson_time[lesson_time],$lesson_days[lesson_days])");
+	$q = mysqli_query($link, "INSERT INTO groups VALUES(null, '$name', '$start_date', 0, '$lesson_time', '$lesson_days')");
 
 }
-echo add_group("group_e,2017-11-31,15:00,+-+++--");
+echo add_group("E", "2017-11-31", "15:00", "+-+++--");
