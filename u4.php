44149826ea7fb1fc486b006d03d13caebaf11876
umar1303 <umarchr@mail.ru>
2017-12-01 16:39:47 +0400

    Убрал ретурн

--- u4.php
+++ u4.php
@@ -6,6 +6,6 @@ mysqli_set_charset($link,'utf8');
 function add_group($name,$start_date,$lesson_time,$lesson_days) {
 	global $link;
 	$q = mysqli_query($link, "INSERT INTO groups VALUES($name[name],$start_date[start_date],$lesson_time[lesson_time],$lesson_days[lesson_days])");
-return $q;
+
 }
 echo add_group("group_e,2017-11-31,15:00,+-+++--");
